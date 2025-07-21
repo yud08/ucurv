b424580186afeaff6c48980211a80c4ac1726533
Duy Nguyen <duyn26364@gmail.com>
2025-07-21 11:59:16 +0100

    changes to __init__, workflows and toml

--- ucurv/__init__.py
+++ ucurv/__init__.py
@@ -3,3 +3,15 @@ from .meyerwavelet import *
 from .ucurv import *
 from .zoneplate import *
 
+"""
+This package implements the Uniform Discrete Curvelet Transform as described in "Uniform discrete curvelet transform", TT Nguyen, H Chauris - IEEE transactions on signal processing, 2010.
+You construct a Udct object by providing the shape of the data to be transformed, and then the configuration for the transform, i.e how many directional wedges you wish to have at each axis and each scale.
+Provides a forwards and backwards version of the transform. The backward inverses the forward transform.
+Taken from the README.
+"""
+
+# _version is only created upon package creation
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "0+unknown"
